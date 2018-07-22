8d322453311f392935113b30e3302db2f024c571
danil <danilaeremenko@mail.ru>
2018-07-22 13:39:11 +0300

    Initial commit

---
+++ src/LeaterBot.java
@@ -0,0 +1,27 @@
+import MapObject.*;
+
+
+public class LeaterBot extends MapObject {
+    private GameMap gameMap;
+
+
+    public LeaterBot() {
+
+    }
+
+    //Главный метод бота
+    public String calculateStepsSequence(GameMap gameMap) {
+        StringBuilder result = new StringBuilder();
+
+        NextStep nextStep = NextStep.STAY;
+
+        //ЛОГИКА
+
+        gameMap.moveAllObjects(nextStep);
+
+
+        return result.toString();
+
+    }
+
+}
